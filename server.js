1c274d40cf6ba2da58575b24c8808c2bf6d2387f
Jamie Skinner <skinner520@gmail.com>
2016-06-01 23:23:35 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//FOR BLUEMIX
+const port = process.env.VCAP_APP_PORT || 3000;
+const host = process.env.VCAP_APP_PORT || 'localhost';
+
+app.get('/', (req, res, next)=>{
+  var b = req.body
+  res.send("Hello World!")
+})
